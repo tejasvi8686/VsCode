cafaff8572bbe04d73f3c066005a4272f5db0457
tejasvi <tejasvi@appenuere>
2025-05-07 00:22:28 +0530

    Integrate Clerk and Convex for user authentication and data management. Updated package dependencies, added middleware for Clerk, and created Convex function structure with example usage in README. Enhanced layout and page components to support user sign-in and display user information.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+import { SignedOut, UserButton } from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
+import { SignedIn } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
+  const { user } = useUser();
+  console.log("user", user);
   return (
     <div>
       <h1>Hello World</h1>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+
+      <div>
+        {user?.emailAddresses[0].emailAddress}
+       
+      </div>
     </div>
+    
   );
 }
